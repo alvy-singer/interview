df6f461e201d295aea96448228f13a5bbb4d4830
yangle <yangle@luojilab.com>
2019-05-08 23:00:26 +0800

    linked list

--- linked_list/linked_list_test.py
+++ linked_list/linked_list_test.py
@@ -1,5 +1,5 @@
 from unittest import TestCase, main
-from linked_list import DoubleLinkedList
+from linked_list import DoubleLinkedList, LinkedList
 
 
 class DoubleLinkedListTest(TestCase):
@@ -8,6 +8,19 @@ class DoubleLinkedListTest(TestCase):
         rv = linked_list.remove_value(1)
         self.assertEqual(rv, linked_list.head)
         self.assertEqual(rv.value, 2)
+
+class LinkedListTest(TestCase):
+    """测试链表
+    """
+
+    def test_add(self):
+        pass
+
+    def test_delete(self):
+        pass
+
+    def test_find(self):
+        pass
 
 
 if __name__ == '__main__':
